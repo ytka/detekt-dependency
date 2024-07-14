6132f7f2b74d3466500e949d7f233d303c37c1e7
ytka <yutakayy@gmail.com>
2024-07-14 19:01:11 +0900

    refactor

--- src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictionRule.kt
+++ src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictionRule.kt
@@ -6,15 +6,15 @@ import io.ytka.packagerestriction.import.*
 import org.jetbrains.kotlin.psi.KtFile
 
 
-private fun createImportRestriction(name: String, importAllows: Any?, importDenys: Any?): ImportRestriction {
+private fun createImportRestriction(name: String, importAllows: List<String>?, importDenys: List<String>?): ImportRestriction {
     if (importAllows != null && importDenys != null) {
         throw IllegalArgumentException("Cannot have both import-allows and import-denys")
     }
-    if (importAllows != null && importAllows is List<*>) {
-        return ImportRestrictionAllow(name, importAllows as List<String>)
+    if (importAllows != null) {
+        return ImportRestrictionAllow(name, importAllows)
     }
-    if (importDenys != null && importDenys is List<*>) {
-        return ImportRestrictionDeny(name, importDenys as List<String>)
+    if (importDenys != null) {
+        return ImportRestrictionDeny(name, importDenys)
     }
     return ImportRestrictionDeny(name, emptyList())
 }
@@ -27,7 +27,7 @@ class PackageRestrictionRule(config: Config) : Rule(config) {
         "Package restriction violation.",
         Debt.TWENTY_MINS)
 
-    private val packagePrefix = valueOrDefault("package-prefix", "unknown")
+    private val projectPackagePrefix = valueOrDefault("project-package-prefix", "")
     private val packageRestrictionSet = PackageRestrictionSet()
     //private val threshold = 10
     //private var amount: Int = 0
@@ -36,9 +36,10 @@ class PackageRestrictionRule(config: Config) : Rule(config) {
         val pkgs = valueOrNull<List<Map<String,Any>>>("packages")
         pkgs?.forEach() {
             val name = it["name"]
-            val importAllows = it["import-allows"]
-            val importDenys = it["import-denys"]
-            println("name: $name, importAllows: $importAllows, importDenys: $importDenys")
+            @Suppress("UNCHECKED_CAST")
+            val importAllows = it["import-allows"] as List<String>?
+            @Suppress("UNCHECKED_CAST")
+            val importDenys = it["import-denys"] as List<String>?
             val importRestriction = createImportRestriction(name as String, importAllows, importDenys)
             packageRestrictionSet.addPackageRestriction(PackagePathFilter(name), importRestriction)
         }
@@ -46,18 +47,13 @@ class PackageRestrictionRule(config: Config) : Rule(config) {
 
     override fun visitKtFile(file: KtFile) {
         super.visitKtFile(file)
-
-        //println("----")
-        //println(file.absolutePath())
-        //println(file.importDirectives.map { it.importedFqName?.asString() })
-
-        val sourcePkg = file.packageFqName.asString()
+        val sourcePkg = file.packageFqName.toString()
         val pkgRestrictions = packageRestrictionSet.findPackageRestrictions(sourcePkg)
 
         file.importDirectives.forEach { importDirective ->
-//            val destPkg = importDirective.importedFqName?.parent()?.asString()
-            val destPkg = importDirective.importedFqName.toString()
-            println("sourcePkg: $sourcePkg, destPkg: $destPkg")
+            val destPkg = importDirective.importedFqName?.parent()?.asString() ?: ""
+//            val destPkg = importDirective.importedFqName.toString()
+            //println("sourcePkg: $sourcePkg, destPkg: $destPkg")
 
             pkgRestrictions.forEach {
                 if (!it.importRestriction.isAllowed(sourcePkg, destPkg)) {
---
+++ src/test/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictionRuleTest.kt
@@ -0,0 +1,9 @@
+package io.ytka.packagerestriction.detektext.rules
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PackageRestrictionRuleTest
