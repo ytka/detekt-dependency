470b5c6ebf91c342b0a8ba442395321b50509a3d
ytka <yutakayy@gmail.com>
2024-07-14 22:18:03 +0900

    add cyclomatic-complex-threshold

--- src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictionRule.kt
+++ src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictionRule.kt
@@ -1,8 +1,9 @@
 package io.ytka.packagerestriction.detektext.rules
 
 import io.gitlab.arturbosch.detekt.api.*
-import io.ytka.packagerestriction.imports.*
+import io.ytka.packagerestriction.detektext.metrics.CyclomaticComplexMethod
 import org.jetbrains.kotlin.psi.KtImportList
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 class PackageRestrictionRule(config: Config) : Rule(config) {
     override val issue = Issue(javaClass.simpleName,
@@ -11,27 +12,25 @@ class PackageRestrictionRule(config: Config) : Rule(config) {
         Debt.TWENTY_MINS)
 
     private val projectPackagePrefix = valueOrDefault("project-package-prefix", "")
-    private val packageRestrictionSet = PackageRestrictions(projectPackagePrefix)
-    //private val threshold = 10
-    //private var amount: Int = 0
+    private val packageRestrictions = PackageRestrictions(projectPackagePrefix)
 
     init {
         val pkgs = valueOrNull<List<Map<String,Any>>>("packages")?: listOf()
-        packageRestrictionSet.append(pkgs)
+        packageRestrictions.append(pkgs)
     }
 
     override fun visitImportList(importList: KtImportList) {
         super.visitImportList(importList)
 
         val sourcePkg = importList.containingKtFile.packageFqName.toString()
-        val pkgRestrictions = packageRestrictionSet.findPackageRestrictions(sourcePkg)
+        val pkgRestrictions = packageRestrictions.findPackageRestrictions(sourcePkg)
 
         val destPkgs = importList.imports.map { importDirective -> importDirective.importPath?.pathStr ?: ""}
             .distinct()
         destPkgs.forEach { destPkg ->
             //println("sourcePkg: $sourcePkg, destPkg: $destPkg")
             pkgRestrictions.forEach { pkgRestriction ->
-                if (!pkgRestriction.isAllowedImport(sourcePkg, destPkg)) {
+                if (!pkgRestriction.importRestriction.isAllowedImport(sourcePkg, destPkg)) {
                     report(CodeSmell(issue, Entity.from(importList),
                         "The import of $destPkg is not allowed from $sourcePkg. details: $pkgRestriction."))
                 }
@@ -39,13 +38,14 @@ class PackageRestrictionRule(config: Config) : Rule(config) {
         }
     }
 
-
-    /*
-    // TODO: calculate cyclomatic complexity
     override fun visitNamedFunction(function: KtNamedFunction) {
         super.visitNamedFunction(function)
-        amount++
-    }
 
-     */
+        val sourcePkg = function.containingKtFile.packageFqName.toString()
+        val pkgRestrictions = packageRestrictions.findPackageRestrictions(sourcePkg)
+
+        pkgRestrictions.forEach { pkgRestriction ->
+            pkgRestriction.cyclomaticComplexMethod.visitNamedFunction(function)?.let { report(it) }
+        }
+    }
 }
--- src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictions.kt
+++ src/main/kotlin/io/ytka/packagerestriction/detektext/rules/PackageRestrictions.kt
@@ -1,10 +1,16 @@
 package io.ytka.packagerestriction.detektext.rules
 
+import io.ytka.packagerestriction.detektext.metrics.CyclomaticComplexMethod
+import io.ytka.packagerestriction.detektext.metrics.CyclomaticComplexity
 import io.ytka.packagerestriction.imports.*
 
+class PackageRestriction(
+    val importRestriction: ImportRestrictionInProject,
+    val cyclomaticComplexMethod: CyclomaticComplexMethod,
+)
 
 class PackageRestrictions(private val projectPackagePrefix: String) {
-    fun findPackageRestrictions(packagePath: PackagePath): List<ImportRestrictionInProject> {
+    fun findPackageRestrictions(packagePath: PackagePath): List<PackageRestriction> {
         if (!packagePath.startsWith(projectPackagePrefix)) {
             return emptyList()
         }
@@ -18,14 +24,23 @@ class PackageRestrictions(private val projectPackagePrefix: String) {
             val importAllows = pkg["import-allows"] as List<String>?
             @Suppress("UNCHECKED_CAST")
             val importDenys = pkg["import-denys"] as List<String>?
+            val cyclomaticComplexityThreshold = pkg["cyclomatic-complex-threshold"] as Int? ?: 15
+            // println("name: $name, importAllows: $importAllows, importDenys: $importDenys, cyclomaticComplexityThreshold: $cyclomaticComplexityThreshold")
+
             val importRestriction = createImportRestriction(name as String, importAllows, importDenys)
-            this.addPackageRestriction(PackagePathFilter(name), importRestriction)
+            val cyclomaticComplexMethod = CyclomaticComplexMethod(
+                threshold = cyclomaticComplexityThreshold,
+            )
+            this.addPackageRestriction(PackagePathFilter(name), importRestriction, cyclomaticComplexMethod)
         }
     }
 
-    private val packageRestrictionSets: MutableMap<PackagePathFilter, ImportRestrictionInProject> = mutableMapOf()
-    private fun addPackageRestriction(pathFilter: PackagePathFilter, importRestriction: ImportRestriction) {
-        packageRestrictionSets[pathFilter] = ImportRestrictionInProject(projectPackagePrefix, pathFilter, importRestriction)
+    private val packageRestrictionSets: MutableMap<PackagePathFilter, PackageRestriction> = mutableMapOf()
+    private fun addPackageRestriction(pathFilter: PackagePathFilter, importRestriction: ImportRestriction, cyclomaticComplexMethod: CyclomaticComplexMethod) {
+        packageRestrictionSets[pathFilter] = PackageRestriction(
+            importRestriction = ImportRestrictionInProject(projectPackagePrefix, pathFilter, importRestriction),
+            cyclomaticComplexMethod = cyclomaticComplexMethod,
+        )
     }
 }
 
